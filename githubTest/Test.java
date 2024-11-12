271d06fc94166b5d56dc796bef8b6e45d7996bf2
hao-xiao-zi <3425588767@qq.com>
2024-11-12 11:39:11 +0800

    测试

---
+++ githubTest/Test.java
@@ -0,0 +1,11 @@
+/**
+* Created with IntelliJ IDEA.
+* Description:
+* User: 34255
+* Date: 2024-11-12
+* Time: 11:37
+*/public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
